ff32b59cfd3bbb18f4d43cd2b214f2d0266eee26
voiddecay <xxxfirdavs5@gmail.com>
2026-07-29 06:01:30 -0400

    Add sysonfo script

---
+++ sysinfo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "===== SYSTEM INFO ====="
+
+echo "User: $USER"
+
+echo "Hostname: $(hostname)"
+
+echo "Current directory: $(pwd)"
+
+echo "Date: $(date)"
+
+echo "Kernel: $(uname -r)"
+
+echo "======================="
